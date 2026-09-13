46d515595c739ba2a7029599afa52a141c621fcc
Shivam2890 <Shivamkori2890@gmail.com>
2026-09-13 15:40:47 +0530

    debounce with useRef

--- src/Namaste Dev/Debounce.jsx
+++ src/Namaste Dev/Debounce.jsx
@@ -1,4 +1,51 @@
-import React, { useCallback, useState } from 'react'
+// import React, { useCallback, useState } from 'react'
+
+// const Debounce = () => {
+//     const [normal, setNormal] = useState("")
+//     const [debounce, setDebounce] = useState("")
+
+
+//     //debounce Fn (main)
+//     function debounceFn(fn, t = 1000) {
+//         let delay
+//         return function (...args) {
+//             clearTimeout(delay)
+//             delay = setTimeout(() => fn(...args), t)
+//         }
+//     }
+
+//     //useCallback
+//     const debounceRequest = useCallback((val) => {
+//         handleDebounce(val)
+//     }, [])
+
+//     //input handling
+//     function handleChange(e) {
+//         setNormal(e.target.value)
+
+//         //passing the input value in the useCallback fn
+//         debounceRequest(e.target.value)
+//     }
+
+//     //here calling the debounceFn by passing in the useCallback
+//     const handleDebounce = debounceFn((val) => setDebounce(val), 1000)
+
+//     return (
+//         <div>
+//             <center>
+//                 <input type="text" onChange={(e) => handleChange(e)} />
+//                 <h2>NORMAL : {normal}</h2>
+//                 <h2>DEBOUNCE : {debounce}</h2>
+//             </center>
+//         </div>
+//     )
+// }
+
+// export default Debounce
+
+//second with the useRef so the debounce fn is persit across the renders
+
+import React, { useCallback, useRef, useState } from 'react'
 
 const Debounce = () => {
     const [normal, setNormal] = useState("")
@@ -14,21 +61,18 @@ const Debounce = () => {
         }
     }
 
-    //useCallback
-    const debounceRequest = useCallback((val) => {
-        handleDebounce(val)
-    }, [])
+
 
     //input handling
     function handleChange(e) {
         setNormal(e.target.value)
 
-        //passing the input value in the useCallback fn
-        debounceRequest(e.target.value)
+
+        handleDebounce.current(e.target.value)
     }
 
     //here calling the debounceFn by passing in the useCallback
-    const handleDebounce = debounceFn((val) => setDebounce(val), 1000)
+    const handleDebounce = useRef(debounceFn((val) => setDebounce(val), 1000))
 
     return (
         <div>
