af5357cf9276cf80f4437789832a45067856d7d0
Shivam2890 <Shivamkori2890@gmail.com>
2026-09-13 17:47:28 +0530

    added transition to avoid every repaint in the browser

--- src/Namaste Dev/ProgressBar1.jsx
+++ src/Namaste Dev/ProgressBar1.jsx
@@ -6,20 +6,24 @@ const ProgressBarComponent = ({ progress }) => {
             width: '500px',
             border: '2px solid black',
             height: '30px',
-            borderRadius: '10px'
+            borderRadius: '10px',
+            overflow: 'hidden'
 
         }}
             //aisstive intervnet for the screen readers
             role='progressbar'
             aria-valuenow={progress}
-            aria-valuemax='100'
-            aria-valuemin='0'
+            aria-valuemax={100}
+            aria-valuemin={0}
         >
             <div style={{
-                width: `${progress}%`,
+                // width: `${progress}%`,
+                transform: `translateX(${progress - 100}%)`,
                 height: '30px',
                 backgroundColor: 'green',
-                borderRadius: '10px'
+                borderRadius: '10px',
+                textAlign: 'right',
+                transition: '0.5s ease-in'
             }}>
                 {progress}
             </div>
