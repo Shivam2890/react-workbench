95b8bffce877c38ac83b60049c4b5b4db45a522a
Shivam2890 <Shivamkori2890@gmail.com>
2026-09-24 15:56:19 +0530

    polyfills inlcudes handling the negative case if too negative then set it to 0

--- src/Polyfills/includes.js
+++ src/Polyfills/includes.js
@@ -4,6 +4,9 @@ Array.prototype.myincludes = function (element, startIndex) {
     if (arguments.length >= 2) {
         index = startIndex
     }
+    if (startIndex < 0) {
+        index = Math.max(this.length + startIndex, 0)
+    }
 
     for (let i = index; i < this.length; i++) {
         if (element === this[i]
@@ -15,7 +18,11 @@ Array.prototype.myincludes = function (element, startIndex) {
     return false
 }
 
-const isThere = [1, 3, 4, 5, NaN].myincludes(NaN)
+const isThere = [1, 3, 4, 5].includes(3, -124)
 
 console.log(isThere) //true for the NaN
 console.log(NaN === NaN)
+
+//   index = Math.max(this.length + startIndex, 0) // if my starting index is too negative (exceeding the length) then it treat as the 0
+
+
